85bd6db6cff45986df8de6f5bff4e37521cf4e98
Farhoe <matej.dalekorej@student.ossp.cz>
2018-11-15 10:05:37 +0100

    It's the right one now
    Tak jdeme na to :)

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Vybírání autíčka</h1>
+
+  <form class="" action="submit.php" method="post">
+  <label for="spd">SPD</label>
+  <input id="spd" type="text" name="spd" placeholder="SPD je moje oblíbená politická strana">
+<br />
+  <label for="auta">Auto</label>
+  <select for="auta" name="auta">
+    <option value="hitlerovo-auto">Hitlerovo auto</option>
+    <option value="ford">Ford</option>
+    <option value="mini">Mini</option>
+    <option value="fiat">Fiat</option>
+    <option value="jeep"></option>
+    <option value="skoda">Škoda</option>
+  </select>
+ <br />
+
+  <input type="submit" name="submit" value="Posílám ti to na tady máš">
+  </form>
+  </body>
+</html>
---
+++ submit.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Auta</h1>
+    <?php
+    $submit=filter_input(input_post,'submit');
+    $auta=filter_input(input_post,'auta');
+    $rok=filter_input(input_post,'rok')
+    $spd=filter_input(input_post,'spd');
+     ?>
+
+
+
+
+  </body>
+</html>
